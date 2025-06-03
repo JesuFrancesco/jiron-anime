a4f544f74db1e20c54c5e82fb9b531d725fd8a72
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2025-06-03 14:40:39 -0500

    fix: error de tipado en parseQueryParams

--- packages/server/prisma/generator/src/copy/parseQueryParams.ts
+++ packages/server/prisma/generator/src/copy/parseQueryParams.ts
@@ -1,10 +1,10 @@
-import { isObject } from './misc'
-import { ParsedQs } from 'qs'
+import { isObject } from "./misc";
+import { ParsedQs } from "qs";
 
 /**
  * Type definition for possible query parameter types.
  */
-type QueryParams = string | ParsedQs | string[] | ParsedQs[] | undefined
+type QueryParams = string | ParsedQs | string[] | ParsedQs[] | undefined;
 
 /**
  * Parses a query value to convert strings to their respective types.
@@ -12,12 +12,12 @@ type QueryParams = string | ParsedQs | string[] | ParsedQs[] | undefined
  * @returns {unknown} The parsed value.
  */
 const parseQueryValue = (value: string): unknown => {
-  if (value === 'true') return true
-  if (value === 'false') return false
-  if (value === 'null') return null
-  if (!isNaN(Number(value))) return Number(value)
-  return value
-}
+  if (value === "true") return true;
+  if (value === "false") return false;
+  if (value === "null") return null;
+  if (!isNaN(Number(value))) return Number(value);
+  return value;
+};
 
 /**
  * Recursively parses query parameters to convert strings to their respective types.
@@ -25,18 +25,18 @@ const parseQueryValue = (value: string): unknown => {
  * @returns {unknown} The parsed query parameters.
  */
 export const parseQueryParams = (params: QueryParams): unknown => {
-  if (typeof params === 'string') {
-    return parseQueryValue(params)
+  if (typeof params === "string") {
+    return parseQueryValue(params);
   }
   if (Array.isArray(params)) {
-    return params.map(parseQueryParams)
+    return params.map(parseQueryParams);
   }
   if (isObject(params)) {
-    const parsedParams: Record<string, unknown> = {}
+    const parsedParams: Record<string, unknown> = {};
     for (const key in params) {
-      parsedParams[key] = parseQueryParams(params[key])
+      parsedParams[key] = parseQueryParams(params[key] as QueryParams);
     }
-    return parsedParams
+    return parsedParams;
   }
-  return params
-}
+  return params;
+};
