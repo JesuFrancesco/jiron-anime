5ba4bcb8e6657ca500ec0496fab036026fe04f83
Rodrigo Pérez <rodrigop_23@outlook.es>
2025-06-16 15:01:59 -0500

    feat: agregar tablas para eventos, migración y seed de event_types

---
+++ packages/server/prisma/seeders/event_types.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    event_types (id, name, active)
+VALUES
+    (1, 'Lanzamientos', true),
+    (2, 'Firma de autores', true),
+    (3, 'Torneos', true),
+    (4, 'Club de lectura', true),
+    (5, 'Subastas', true),
+    (6, 'Talleres', true),
+    (7, 'Otros', true)
